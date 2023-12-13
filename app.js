27ebcef679887b75c31431d6e74f1e1deeda2fa4
chrislwebdev <chrislwebdev@gmail.com>
2023-12-13 12:23:30 +0300

    A list of minor CSS changes

--- app.js
+++ app.js
@@ -14,29 +14,3 @@ navClose.addEventListener("click", () => {
   navHeader.classList.remove("nav-header-disappear");
   navContainer.classList.remove("navbar-reveal");
 });
-
-// window.addEventListener("resize", () => {
-//   console.log(window.innerWidth);
-// });
-
-// 2. Cards //
-
-// Image open
-// const cardBtn = document.querySelectorAll(".card-btn");
-
-// cardBtn.forEach(function (card) {
-//   card.addEventListener("click", (e) => {
-//     var windowWidth = screen.width;
-//     var windowHeight = screen.height;
-
-//     var newWindow = window.open(
-//       e.target.href,
-//       "_blank",
-//       "width=" +
-//         windowWidth +
-//         "height=" +
-//         windowHeight +
-//         "toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes"
-//     );
-//   });
-// });
